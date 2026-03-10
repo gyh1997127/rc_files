c31e2599ab8fb47b4c1777bf5f01062b2fb6f639
Yuhui Gao <ygao@andestech.com>
2026-03-10 10:49:54 -0700

    fix

--- nvim/lua/plugin/gutentags.lua
+++ nvim/lua/plugin/gutentags.lua
@@ -9,7 +9,7 @@ return {
 
     -- Prevent gutentags from creating a 'tags' file in every subdirectory
     vim.g.gutentags_add_default_project_roots = 0
-    vim.g.gutentags_project_root = { ".git", ".slang", "tags" }
+    vim.g.gutentags_project_root = { ".git" }
     vim.g.gutentags_cache_dir = vim.fn.stdpath("cache") .. "/tags"
 
     -- Trigger updates on save
